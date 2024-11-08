6806867333e908132e22f398641d08d65149a821
Vincent Yang <48404862+Vincent550102@users.noreply.github.com>
2024-11-08 20:34:36 +0800

    Update typo

--- ELK_auto_newline.js
+++ ELK_auto_newline.js
@@ -4,7 +4,7 @@
 // @version      2024-08-03
 // @description  try to take over the world!
 // @author       You
-// @match        http://elk.hitcon/app/discover
+// @match        http://elk.hitcon:5601/app/discover
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // @run-at document-end
