5be459afb54e8c88a27684b3314ef2113198e580
Vincent Yang <48404862+Vincent550102@users.noreply.github.com>
2024-11-08 16:54:05 +0800

    Update ELK_auto_newline.js

--- ELK_auto_newline.js
+++ ELK_auto_newline.js
@@ -4,7 +4,7 @@
 // @version      2024-08-03
 // @description  try to take over the world!
 // @author       You
-// @match        https://elk.itiscaleb.com/app/discover
+// @match        http://elk.hitcon/app/discover
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // @run-at document-end
